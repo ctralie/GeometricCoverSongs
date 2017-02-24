e01106b5575a14b0336d1c2364ce5faa661e115c
Chris Tralie <chris.tralie@gmail.com>
2017-02-24 09:37:49 -0500

    Progress on porting algorithms over to parallel

--- ParallelAlgorithms.py
+++ ParallelAlgorithms.py
@@ -1,14 +1,20 @@
 import pycuda.autoinit
 import pycuda.driver as drv
 import pycuda.gpuarray as gpuarray
+import pycuda.cumath
+import skcuda.misc
+import skcuda.linalg as linalg
 import numpy as np
 import matplotlib.pyplot as plt
 import time
 import scipy.io as sio
+from CSMSSMTools import *
 
 from pycuda.compiler import SourceModule
 
 bitonicSort_ = None
+getSumSquares_ = None
+finishCSM_ = None
 
 def initParallelAlgorithms():
     global bitonicSort_
@@ -17,13 +23,31 @@ def initParallelAlgorithms():
     fin.close()
     bitonicSort_ = mod.get_function("bitonicSort")
 
+    global finishCSM_
+    global getSumSquares_
+    fin = open("ParallelAlgorithms/CSMHelper.cu")
+    mod = SourceModule(fin.read())
+    fin.close()
+    finishCSM_ = mod.get_function("finishCSM")
+    getSumSquares_ = mod.get_function("getSumSquares")
+
     #Run each of the algorithms on dummy data so that they're pre-compiled
+
+    #1) Bitonic Sort
     X = np.random.randn(16, 16)
     N = np.int32(16)
     NPow2 = N
     NThreads = N/2
     XG = gpuarray.to_gpu(X)
     bitonicSort_(XG, N, NPow2, block=(NThreads, 1, 1), grid=(X.shape[0], 1), shared=4*NPow2)
+
+    linalg.init()
+    #2) Other primitive operations
+    NegXDotX = linalg.dot(XG, XG)
+    XPlusX = skcuda.misc.add(XG, XG)
+    XSqr = skcuda.misc.multiply(XG, XG)
+    XSqr = skcuda.misc.sum(XSqr, 1)
+    XPlusCol = skcuda.misc.add_matvec(XG, XSqr, 0)
 
 def bitonicSort(XG):
     N = np.int32(XG.shape[1])
@@ -32,41 +56,7 @@ def bitonicSort(XG):
     NThreads = min(N2, 512)
     bitonicSort_(XG, N, NPow2, block=(NThreads, 1, 1), grid=(XG.shape[0], 1), shared=4*NPow2)
 
-def testBitonicSortTimeRatios(sizes, NTrials):
-    initParallelAlgorithms()
-    np.random.seed(100)
-    CPUTimes = np.zeros((len(sizes), NTrials))
-    GPUTimes = np.zeros((len(sizes), NTrials))
-    for i in range(len(sizes)):
-        N = sizes[i]
-        for t in range(NTrials):
-            X = np.array(np.random.rand(N, N), dtype=np.float32)
-            XG = gpuarray.to_gpu(X)
-
-            tic = time.time()
-            bitonicSort(XG)
-            toc = time.time()
-            GPUTime = toc-tic
-
-            tic = time.time()
-            X2 = np.sort(X, 1)
-            toc = time.time()
-            CPUTime = toc-tic
-
-            print("N = %i"%N)
-            print("Elapsed Time CPU: %g"%CPUTime)
-            print("Elapsed Time GPU: %g (Ratio %.3g)"%(GPUTime, CPUTime/GPUTime))
-            CPUTimes[i, t] = CPUTime
-            GPUTimes[i, t] = GPUTime
-    sio.savemat("Timings.mat", {"CPUTimes":CPUTimes, "GPUTimes":GPUTimes})
-
-if __name__ == '__main__':
-    testBitonicSortTimeRatios(range(10, 3000, 10), 10)
-
-if __name__ == '__main__2':
-    initParallelAlgorithms()
-    np.random.seed(100)
-    N = 3000
+def testBitonicSort(N, doPlot = False):
     X = np.array(np.random.rand(N, N), dtype=np.float32)
     XG = gpuarray.to_gpu(X)
 
@@ -80,15 +70,102 @@ if __name__ == '__main__2':
     toc = time.time()
     CPUTime = toc-tic
 
+    print("N = %i"%N)
     print("Elapsed Time CPU: %g"%CPUTime)
     print("Elapsed Time GPU: %g (Ratio %.3g)"%(GPUTime, CPUTime/GPUTime))
+    print("AllClose: ", np.allclose(np.sort(X, 1), XG.get()))
 
-    plt.subplot(121)
-    plt.imshow(X, interpolation = 'none')
-    plt.subplot(122)
-    plt.imshow(XG.get(), interpolation = 'none')
-    #plt.subplot(133)
-    XDiff = X - XG.get()
-    #plt.imshow(XDiff, interpolation = 'none')
-    #print np.max(XDiff)
+    if doPlot:
+        plt.subplot(121)
+        plt.imshow(X, interpolation = 'none')
+        plt.subplot(122)
+        plt.imshow(XG.get(), interpolation = 'none')
+        plt.show()
+    return (CPUTime, GPUTime)
+
+def testBitonicSortTimeRatios(sizes, NTrials):
+    np.random.seed(100)
+    CPUTimes = np.zeros((len(sizes), NTrials))
+    GPUTimes = np.zeros((len(sizes), NTrials))
+    for i in range(len(sizes)):
+        N = sizes[i]
+        for t in range(NTrials):
+            (CPUTimes[i, t], GPUTimes[i, t]) = testBitonicSort(N)
+    sio.savemat("Timings.mat", {"CPUTimes":CPUTimes, "GPUTimes":GPUTimes})
+
+def roundUpPow2(x):
+    return np.array(int(2**np.ceil(np.log2(float(x)))), dtype=np.int32)
+
+def getCSMGPU(XG, YG):
+    #YGT = linalg.transpose(YG)
+    x = 1
+
+def testCSM(M, N, NOthers):
+    X = np.array(np.random.randn(M, 25*25), dtype = np.float32)
+    Y = np.array(np.random.randn(N*NOthers, 25*25), dtype = np.float32)
+
+    tic = time.time()
+    XG = gpuarray.to_gpu(X)
+    YG = gpuarray.to_gpu(Y)
+    XSqr = gpuarray.to_gpu(np.array(np.zeros(X.shape[0]), dtype=np.float32))
+    YSqr = gpuarray.to_gpu(np.array(np.zeros(Y.shape[0]), dtype=np.float32))
+    toc = time.time()
+    print "GPU Copy Time: ", toc - tic
+
+    tic = time.time()
+    CSM1 = getCSM(Y, X)
+    toc = time.time()
+    CPUTime = toc-tic
+
+    ticg = time.time()
+
+    #Step 1: Sum of squares across rows
+    dim = np.array(X.shape[1], dtype=np.int32)
+    dimpow2 = roundUpPow2(dim)
+    NThreads = min(dimpow2, 512)
+    getSumSquares_(YG, YSqr, dim, dimpow2, block=(NThreads, 1, 1), grid=(YG.shape[0], 1), shared=4*dimpow2)
+    getSumSquares_(XG, XSqr, dim, dimpow2, block=(NThreads, 1, 1), grid=(XG.shape[0], 1), shared=4*dimpow2)
+
+    #Step 2: Do multiplication part
+    tic = time.time()
+    XGT = linalg.transpose(XG)
+    CSM = linalg.dot(YG, XGT)
+    print "Elapsed time multiply: ", time.time() - tic
+
+    #Step 3: Add everything together
+    Mp = np.array(XG.shape[0], dtype=np.int32)
+    Np = np.array(YG.shape[0], dtype=np.int32)
+    MPow2 = roundUpPow2(XG.shape[0])
+    #CSM is N x M
+    tic = time.time()
+    finishCSM_(CSM, XSqr, YSqr, Np, Mp, MPow2, block=(int(MPow2), 1, 1), grid=(YG.shape[0], 1))
+    print "Elapsed Time Finish: ", time.time() - toc
+
+
+    tocg = time.time()
+    GPUTime = tocg - ticg
+
+    #print("Elapsed Time CPU: %g"%CPUTime)
+    #print("Elapsed Time GPU: %g"%GPUTime)
+
+    CSM = CSM.get()
+    CSM = CSM[0:N, :]
+    CSM1 = CSM1[0:N, :]
+    plt.subplot(131)
+    plt.imshow(CSM1, interpolation = 'none', cmap = 'afmhot')
+    plt.subplot(132)
+    plt.imshow(CSM, interpolation = 'none', cmap = 'afmhot')
+    plt.subplot(133)
+    plt.imshow(CSM1 - CSM, interpolation = 'none', cmap = 'spectral')
     plt.show()
+
+
+
+if __name__ == '__main__':
+    np.random.seed(100)
+    initParallelAlgorithms()
+    testCSM(1000, 1000, 60)
+
+if __name__ == '__main__2':
+    initParallelAlgorithms()
+    testBitonicSort(3000, True)
---
+++ ParallelAlgorithms/CSMHelper.cu
@@ -0,0 +1,74 @@
+__global__ void getSumSquares(float* X, float* XSqr, int dim, int dimpow2) {
+    extern __shared__ float x[];
+    int i, k;
+    int offset = blockIdx.x*dim;
+    int jump = dimpow2;
+    int sumjump = jump >> 1;
+    //Step 0: Figure out K (number of batches per block)
+    int K = dimpow2 >> 9;
+    if (K == 0) {
+        K = 1;
+    }
+    if (jump > 512) {
+        jump = 512;
+    }
+    //Step 1: Copy over each row to shared memory
+    //and square in the process
+    for (k = 0; k < K; k++) {
+        i = k*jump + threadIdx.x;
+        if (i < dim) {
+            x[i] = X[offset + i]*X[offset + i];
+        }
+        else if (i < dimpow2) {
+            x[i] = 0.0;
+        }
+    }
+    //Step 2: Perform sums
+    while (sumjump > 0) {
+        if (threadIdx.x < sumjump) {
+            K = sumjump >> 9;
+            if (K == 0) {
+                K = 1;
+            }
+            jump = sumjump;
+            if (jump > 512) {
+                jump = 512;
+            }
+            for (k = 0; k < K; k++) {
+                i = k*jump + threadIdx.x;
+                x[i] += x[i + sumjump];
+            }
+        }
+        sumjump = sumjump >> 1;
+        __syncthreads();
+    }
+
+    //Step 3: Copy back results
+    XSqr[blockIdx.x] = x[0];
+}
+
+//CSM is N x M
+__global__ void finishCSM(float* CSM, float* XSqr, float* YSqr, int N, int M, int MPow2) {
+    int offset = blockIdx.x*M;
+    int K = MPow2 >> 9;
+    int i;
+    int k, jump = MPow2;
+    float val = 0.0;
+    if (K == 0) {
+        K = 1;
+    }
+    if (jump > 512) {
+        jump = 512;
+    }
+    for (k = 0; k < K; k++) {
+        i = k*jump + threadIdx.x;
+        if (i < M) {
+            val = XSqr[i] + YSqr[blockIdx.x];
+            val = val - 2*CSM[offset + i];
+            if (val < 0) {
+                val = 0;
+            }
+            CSM[offset + i] = sqrt(val);
+        }
+    }
+}
