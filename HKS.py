efa075b9b87d63ce742cb22320b759ae234e2a96
Chris Tralie <chris.tralie@gmail.com>
2018-07-27 11:07:09 -0400

    Playing around with time HKS

--- HKS.py
+++ HKS.py
@@ -3,6 +3,17 @@ from scipy import sparse
 from scipy.sparse.linalg import lsqr, cg, eigsh
 import matplotlib.pyplot as plt 
 
+def getLaplacianEigs(A, NEigs):
+    DEG = sparse.dia_matrix((A.sum(1).flatten(), 0), A.shape)
+    L = DEG - A
+    w, v = eigsh(L, k=NEigs, sigma = 0, which = 'LM')
+    return (w, v, L)
+
+def getLaplacianEigsDense(A, NEigs):
+    DEG = sparse.dia_matrix((A.sum(1).flatten(), 0), A.shape)
+    L = DEG.toarray() - A
+    w, v = eigsh(L, k=NEigs)
+    return (w[0:NEigs], v[:, 0:NEigs], L)
 
 def getHeat(eigvalues, eigvectors, t, initialVertices, heatValue = 100.0):
     """
@@ -36,7 +47,7 @@ def getHeat(eigvalues, eigvectors, t, initialVertices, heatValue = 100.0):
     heat = eigvectors.dot(coeffs[:, None])
     return heat
 
-def getHKS(eigvalues, eigvectors, ts, scaleinv = True):
+def getHKS(eigvalues, eigvectors, ss, scaleinv = True):
     """
     Given a triangle mesh, approximate its curvature at some measurement scale
     by recording the amount of heat that remains at each vertex after a unit impulse
@@ -49,20 +60,57 @@ def getHKS(eigvalues, eigvectors, ts, scaleinv = True):
     eigvectors : ndarray (N, N)
         Corresponding array of eigenvectors of the graph Laplacian, where
         each eigenvector is along a column
-    ts : ndarray (T, 1)
-        The time scales at which to compute the HKS
+    ss : ndarray (S, 1)
+        The spatial scales at which to compute the HKS
     scaleinv : boolean
         Whether or not to do the scale invariant version where the log is taken
         and the non-DC magnitude Fourier coefficients are reported
     Returns
     -------
-    hks : ndarray (N, T)
-        The heat kernel signature at each point, sampled at T time indices
+    hks : ndarray (N, S)
+        The heat kernel signature at each point, sampled at S spatial indices
     """
-    res = (eigvectors[:, :, None]**2)*np.exp(-eigvalues[None, :, None]*ts[None, None, :])
+    res = (eigvectors[:, :, None]**2)*np.exp(-eigvalues[None, :, None]*ss[None, None, :])
     res = np.sum(res, 1)
     if scaleinv:
         res = np.log(res)
         res = np.abs(np.fft.fft(res, axis=1))
         res = res[:, 1::]
     return res
+
+def getTimeHKS(W, ss, ts, NEigs, scaleinv=False):
+    """
+    Given a triangle mesh, approximate its curvature at some measurement scale
+    by recording the amount of heat that remains at each vertex after a unit impulse
+    of heat is applied.  This is called the "Heat Kernel Signature" (HKS)
+
+    Parameters
+    ----------
+    W : ndarray(N, N)
+        A similarity matrix between all points
+    ss : ndarray (S, 1)
+        The spatial scales at which to compute the HKS
+    ts : ndarray (T, 1)
+        The time scales at which to compute the HKS
+    NEigs : int
+        Number of eigenvectors to use
+    scaleinv : boolean
+        Whether or not to do the scale invariant version where the log is taken
+        and the non-DC magnitude Fourier coefficients are reported
+    Returns
+    -------
+    hks : ndarray (N, S, T)
+        The heat kernel signature at each point, sampled at S spatial indices and T time indices
+    """
+    N = W.shape[0]
+    tdiff = np.linspace(0, 1, N)
+    I, J = np.meshgrid(tdiff, tdiff)
+    tdiffs = np.abs(I-J)
+    S = ss.size
+    T = ts.size
+    res = np.zeros((N, S, T))
+    for i, t in ts:
+        thisW = W*(tdiffs < t)
+        (eigvalues, eigvectors, L) = getLaplacianEigsDense(thisW, NEigs)
+        res[:, :, i] = getHKS(eigvalues, eigvectors, ts, scaleinv=False)
+    return thisW
--- SongStructure.py
+++ SongStructure.py
@@ -8,9 +8,8 @@ import matplotlib.pyplot as plt
 import scipy.io as sio
 import librosa
 import argparse
-from CSMSSMTools import getCSM, getCSMCosine
+from CSMSSMTools import getCSM, getCSMCosine, CSMToBinaryMutual
 from SimilarityFusion import doSimilarityFusion
-from Laplacian import *
 from HKS import *
 from pyMIRBasic.Chroma import *
 from pyMIRBasic.MFCC import *
@@ -36,7 +35,43 @@ def pretty_floats(obj):
         return map(pretty_floats, obj)
     return obj
 
-def exportToLoopDitty(XAudio, Fs, hopSize, winFac, hks, outfilename):
+def getDiffusionMap(W, t = -1, includeDiag = True, thresh = 5e-4, NEigs = 51):
+    """
+    :param W: A similarity matrix
+    :param t: Diffusion parameter.  If -1, do Autotuning
+    :param includeDiag: If true, include recurrence to diagonal in the markov
+        chain.  If false, zero out diagonal
+    :param thresh: Threshold below which to zero out entries in markov chain in
+        the sparse approximation
+    :param NEigs: The number of eigenvectors to use in the approximation
+    """
+    if not includeDiag:
+        np.fill_diagonal(W, np.zeros(W.shape[0]))
+    RowSumSqrt = np.sqrt(np.sum(W, 1))
+    DInvSqrt = sparse.diags([1/RowSumSqrt], [0])
+
+    #Symmetric normalized
+    Pp = (W/RowSumSqrt[None, :])/RowSumSqrt[:, None]
+    Pp[Pp < thresh] = 0
+    Pp = sparse.csr_matrix(Pp)
+
+    lam, X = sparse.linalg.eigsh(Pp, NEigs, which='LM')
+    lam = lam/lam[-1] #In case of numerical instability
+
+    #Check to see if autotuning
+    if t > -1:
+        lamt = lam**t
+    else:
+        #Autotuning diffusion time
+        lamt = np.array(lam)
+        lamt[0:-1] = lam[0:-1]/(1-lam[0:-1])
+
+    #Do eigenvector version
+    V = DInvSqrt.dot(X) #Right eigenvectors
+    M = V*lamt[None, :]
+    return M/RowSumSqrt[:, None] #Put back into orthogonal Euclidean coordinates
+
+def exportToLoopDitty(XAudio, Fs, hopSize, winFac, pX, outfilename):
     if os.path.exists("temp.wav"):
         os.remove("temp.wav")
     sio.wavfile.write("temp.wav", Fs, XAudio)
@@ -50,10 +85,8 @@ def exportToLoopDitty(XAudio, Fs, hopSize, winFac, hks, outfilename):
     mp3data = mp3data.encode("base64")
     fin.close()
 
-    pca = PCA(n_components=3)
-    X = np.zeros((hks.shape[0], 4))
-    X[:, 0:3] = pca.fit_transform(hks)
-
+    X = np.zeros((pX.shape[0], 4))
+    X[:, 0:3] = pX
     interval = hopSize*winFac/float(Fs)
     X[:, -1] = interval*np.arange(X.shape[0])
 
@@ -64,7 +97,7 @@ def exportToLoopDitty(XAudio, Fs, hopSize, winFac, hks, outfilename):
     fout.close()
 
 
-def getFusedSimilarityHKS(XAudio, Fs, hopSize, winFac, winsPerBlock, K, NEigs):
+def getFusedSimilarityHKS(XAudio, Fs, hopSize, winFac, winsPerBlock, K, NEigs, ss):
     """
     Come up with a representation of recurrence based on similarity network fusion (SNF) 
     of averaged/stack delayed Chromas and MFCCs, and compute the HKS of this using
@@ -86,6 +119,8 @@ def getFusedSimilarityHKS(XAudio, Fs, hopSize, winFac, winsPerBlock, K, NEigs):
         Number of nearest neighbors in SNF
     NEigs: int
         Number of eigenvectors to use in HKS
+    ss: ndarray (S, 1)
+        An array of spatial scales at which to sample the HKS
     """
     XChroma = getHPCPEssentia(XAudio, Fs, hopSize*4, hopSize, NChromaBins = 12)
     XMFCC = getMFCCsLibrosa(XAudio, Fs, int(Fs/4), hopSize, lifterexp = 0.6, NMFCC = 20)
@@ -116,29 +151,96 @@ def getFusedSimilarityHKS(XAudio, Fs, hopSize, winFac, winsPerBlock, K, NEigs):
     WFused = doSimilarityFusion(Ds, K = K, NIters = 10, regDiag = 1, regNeighbs=0.0)
     np.fill_diagonal(WFused, 0)
     (eigvalues, eigvectors, L) = getLaplacianEigsDense(WFused, NEigs)
-    hks = getHKS(eigvalues, eigvectors, np.linspace(0, 2, 100), scaleinv=False)
-    return {'hks':hks, 'W':WFused}
+    hks = getHKS(eigvalues, eigvectors, ss, scaleinv=False)
+    return {'hks':hks, 'W':WFused, 'eigvalues':eigvalues, 'eigvectors':eigvectors}
 
 if __name__ == '__main__':
     hopSize=512
     winFac = 5
     winsPerBlock = 20
-    K = 10
-    NEigs = 50
+    K = 20
+    NEigs = 20
 
+    # Come up with log-sampled spatial scale limits
+    lims = [0.1, 2]
+    T = 200
+    alpha = float(lims[1]/lims[0])**(1.0/T)
+    ss = lims[0]*alpha**np.arange(T)
+    # Come up with log-sampled time scale limits
+    ts = np.linspace(0, 1, 100)
+    ss = np.linspace(0, 10, T)
 
+    print("Getting HKS for song 1...")
     XAudio1, Fs = getAudioLibrosa("CSMViewer/MJ.mp3")
-    res = getFusedSimilarityHKS(XAudio1, Fs, hopSize, winFac, winsPerBlock, K, NEigs)
+    res = getFusedSimilarityHKS(XAudio1, Fs, hopSize, winFac, winsPerBlock, K, NEigs, ss)
     hks1 = res['hks']
     W1 = res['W']
     hks1 = np.log(hks1)
+    #hks1 = np.abs(np.fft.fft(hks1, axis=1))[:, 1::]
 
+
+    print("Getting HKS for song 2...")
     XAudio2, Fs = getAudioLibrosa("CSMViewer/AAF.mp3")
-    res = getFusedSimilarityHKS(XAudio2, Fs, hopSize, winFac, winsPerBlock, K, NEigs)
+    res = getFusedSimilarityHKS(XAudio2, Fs, hopSize, winFac, winsPerBlock, K, NEigs, ss)
     hks2 = res['hks']
     W2 = res['W']
     hks2 = np.log(hks2)
+    #hks2 = np.abs(np.fft.fft(hks2, axis=1))[:, 1::]
 
+    sio.savemat('hks.mat', {'hks1':hks1, 'hks2':hks2, 'W1':W1, 'W2':W2})
+
+
+
+
+
+    W = W2
+    N = W.shape[0]
+    tdiff = np.linspace(0, 1, N)
+    I, J = np.meshgrid(tdiff, tdiff)
+    tdiffs = np.abs(I-J)
+    S = ss.size
+    T = ts.size
+    plt.figure(figsize=(12, 12))
+    idxs = [400, 500, 600, 1240]
+    for i, t in enumerate(ts):
+        thisW = W*(tdiffs < t)
+        (eigvalues, eigvectors, L) = getLaplacianEigsDense(thisW, NEigs)
+        hks = getHKS(eigvalues, eigvectors, ss, scaleinv=False)
+        hks = np.log(hks)
+        plt.clf()
+        plt.subplot(221)
+        plt.imshow(thisW, cmap = 'afmhot')
+        plt.title("W")
+        for idx in idxs:
+            plt.scatter(idx, idx, 50)
+        plt.subplot(222)
+        plt.imshow(hks, aspect='auto')
+        plt.title("HKS")
+        plt.subplot(223)
+        for idx in idxs:
+            plt.plot(hks[idx, :])
+        plt.subplot(224)
+        plt.plot(eigvalues)
+        plt.title("Eigenvalues")
+        plt.savefig("%i.png"%i, bbox_inches='tight')
+
+
+    print("Doing Smith Waterman...")
+    Kappa = 0.1
+    CSM = getCSM(hks1, hks2)
+    DBinary = CSMToBinaryMutual(CSM, Kappa)
+    import SequenceAlignment.SequenceAlignment as SA
+    #(maxD, D) = SA.swalignimpconstrained(DBinary)
+    plt.subplot(131)
+    plt.imshow(CSM, interpolation = 'nearest', cmap = 'afmhot')
+    plt.title('CSM')
+    plt.subplot(132)
+    plt.imshow(1-DBinary, interpolation = 'nearest', cmap = 'gray')
+    plt.title("CSM Binary, $\kappa$=%g"%Kappa)
+    plt.subplot(133)
+    #plt.imshow(D, interpolation = 'nearest', cmap = 'afmhot')
+    #plt.title("Smith Waterman Score = %g"%maxD)
+    plt.show()
 
     plt.subplot(221)
     plt.imshow(W1, cmap = 'afmhot', interpolation='none')
@@ -149,5 +251,20 @@ if __name__ == '__main__':
     plt.subplot(224)
     plt.imshow(hks2, cmap = 'afmhot', interpolation='none', aspect='auto')
     plt.show()
-    exportToLoopDitty(XAudio1, Fs, hopSize, winFac, hks1, "MJ.json")
-    exportToLoopDitty(XAudio2, Fs, hopSize, winFac, hks2, "AAF.json")
+
+    pca = PCA(n_components=3)
+    exportToLoopDitty(XAudio1, Fs, hopSize, winFac, pca.fit_transform(hks1), "MJhks.json")
+    exportToLoopDitty(XAudio2, Fs, hopSize, winFac, pca.fit_transform(hks2), "AAFhks.json")
+
+    X1 = getDiffusionMap(W1)
+    X2 = getDiffusionMap(W2)
+    plt.subplot(131)
+    plt.imshow(X1, cmap = 'afmhot', aspect='auto')
+    plt.subplot(132)
+    plt.imshow(X2, cmap = 'afmhot', aspect='auto')
+    plt.subplot(133)
+    plt.imshow(getCSM(X1[:, 0:-1], X2[:, 0:-1]), interpolation = 'nearest', cmap = 'afmhot')
+    plt.show()
+
+    exportToLoopDitty(XAudio1, Fs, hopSize, winFac, X1[:, -4:-1], "MJdiffusion.json")
+    exportToLoopDitty(XAudio2, Fs, hopSize, winFac, X2[:, -4:-1], "AAFdiffusion.json")
